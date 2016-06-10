86d212142ed2ef754c2d053a1811147d2c2fbafc
Jonathon <jonathon.nordquist@gmail.com>
2016-06-10 16:35:15 -0700

    Update charshift for flexability
    Update charshift to use get_shift_position to determine new value regardless of direction.
    Update spec to accomidate looping and negative shifts.

--- lib/charshift.rb
+++ lib/charshift.rb
@@ -3,10 +3,11 @@ require 'helper'
 class String
   def charshift shift_val
     output = ""
-    abs_shift_val = Helper.get_shift_diff(shift_val, 128)
+    # abs_shift_val = Helper.get_shift_diff(shift_val, 128)
     self.split("").each do |char|
       char_ord = Helper.get_ord_by_char(char)
-      output << Helper.get_char_by_ord(char_ord + abs_shift_val)
+      shifted_position = Helper.get_shift_position(char_ord, shift_val)
+      output << Helper.get_char_by_ord(shifted_position)
     end
     return output
   end
--- spec/charshift_spec.rb
+++ spec/charshift_spec.rb
@@ -2,6 +2,7 @@ require 'charshift'
 
 describe 'Charshift gem' do | |
   let(:string1) { "String one" }
+  let(:string2) { "S" }
 
   describe 'charshift method' do 
     it "returns an object of type string" do
@@ -11,5 +12,8 @@ describe 'Charshift gem' do | |
 
   it "shifts all characters the correct number of places on the default ASCII table" do
     expect(string1.charshift(1)).to eq("Tusjoh!pof")
+    expect(string1.charshift(60)).to eq("\u000F0.%*#\\+*!")
+    expect(string2.charshift(-10)).to eq("I")
+    expect(string2.charshift(-110)).to eq("e")
   end
 end
