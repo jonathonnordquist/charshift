83d9ad5eea97970194b57c9ff41526509b623858
Jonathon <jonathon.nordquist@gmail.com>
2016-06-10 10:47:34 -0700

    Set confirm_int method to return true for integer
    Modify confirm_int type error test to check for non fixnum values (decimals.)
    Set confirm_int to return true for fixnum types.
    Create test for fixnum values.

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,8 +1,10 @@
 module Helper
 
   def confirm_int input_val
-    if !input_val.instance_of? Integer
+    if !input_val.instance_of? Fixnum
       raise TypeError, 'Input value must be of type integer'
+    else
+      return true
     end
   end
 end
--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,14 +1,23 @@
 require 'helper'
 
 describe 'Helper module' do
-    let(:including_class) { Class.new { extend Helper } }
+  let(:including_class) { Class.new { extend Helper } }
 
-   describe 'confirm_int method' do
+  describe 'confirm_int method' do
+    let(:type_error) { 'Input value must be of type integer' }
 
     it 'raises an exception if it does not recieve an object of type int' do
       expect {
         including_class.confirm_int("aaaa")
-      }.to raise_error('Input value must be of type integer')
+      }.to raise_error(type_error)
+
+      expect {
+        including_class.confirm_int(0.7)
+      }.to raise_error(type_error)
+    end
+
+    it 'returns true if it recieves an object of type int' do
+      expect(including_class.confirm_int(7)).to be true
     end
   end
 end
