485cc06c1e5c9bee5fa6ece7efdff07b94dbb842
Jonathon <jonathon.nordquist@gmail.com>
2016-06-10 19:29:46 -0700

    Add additional tests for charshift method

--- lib/charshift.rb
+++ lib/charshift.rb
@@ -3,7 +3,6 @@ require 'helper'
 class String
   def charshift shift_val
     output = ""
-    # abs_shift_val = Helper.get_shift_diff(shift_val, 128)
     self.split("").each do |char|
       char_ord = Helper.get_ord_by_char(char)
       shifted_position = Helper.get_shift_position(char_ord, shift_val)
--- spec/charshift_spec.rb
+++ spec/charshift_spec.rb
@@ -13,6 +13,10 @@ describe 'Charshift gem' do | |
   it "shifts all characters the correct number of places on the default ASCII table" do
     expect(string1.charshift(1)).to eq("Tusjoh!pof")
     expect(string1.charshift(60)).to eq("\u000F0.%*#\\+*!")
+    expect(string1.charshift(-1)).to eq("Rsqhmf\x1Fnmd")
+    expect(string1.charshift(-60)).to eq("\x1786-2+d32)")
+    expect(string2.charshift(10)).to eq("]")
+    expect(string2.charshift(110)).to eq("A")
     expect(string2.charshift(-10)).to eq("I")
     expect(string2.charshift(-110)).to eq("e")
   end
