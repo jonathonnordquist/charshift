35e799f64a4af448dc4f121961a83e4bcf61fdc9
Jonathon <jonathon.nordquist@gmail.com>
2016-06-13 12:10:26 -0700

    Update var names in charshift method

--- lib/charshift.rb
+++ lib/charshift.rb
@@ -1,11 +1,11 @@
 require 'charshifthelper'
 
 class String
-  def charshift shift_val
+  def charshift char_shift_val
     output = ""
     self.split("").each do |char|
-      char_ord = CharshiftHelper.get_ord_by_char(char)
-      shifted_position = CharshiftHelper.get_shift_position(char_ord, shift_val)
+      current_char_ord = CharshiftHelper.get_ord_by_char(char)
+      shifted_position = CharshiftHelper.get_shift_position(current_char_ord, char_shift_val)
       output << CharshiftHelper.get_char_by_ord(shifted_position)
     end
     return output
