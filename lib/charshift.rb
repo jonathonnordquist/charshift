c4938bc2ef090653fa0d80751520b57f67332244
Jonathon <jonathon.nordquist@gmail.com>
2016-06-13 12:29:39 -0700

    Add error handling to charshift
    Create begin/recuse/else for charshift.  Use CharshiftHelper.confirm_int
    to check for correct input value and throw error if value is not fixnum.

--- lib/charshift.rb
+++ lib/charshift.rb
@@ -2,12 +2,19 @@ require 'charshifthelper'
 
 class String
   def charshift char_shift_val
-    output = ""
-    self.split("").each do |char|
-      current_char_ord = CharshiftHelper.get_ord_by_char(char)
-      shifted_position = CharshiftHelper.get_shift_position(current_char_ord, char_shift_val)
-      output << CharshiftHelper.get_char_by_ord(shifted_position)
+    begin
+      CharshiftHelper.confirm_int(char_shift_val)
+    rescue TypeError => e
+      e
+    else
+      output = ""
+      self.split("").each do |char|
+        current_char_ord = CharshiftHelper.get_ord_by_char(char)
+        shifted_position = CharshiftHelper.get_shift_position(current_char_ord, char_shift_val)
+        output << CharshiftHelper.get_char_by_ord(shifted_position)
+      end
+      return output
     end
-    return output
+      
   end
 end
